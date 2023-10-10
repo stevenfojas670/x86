aa7281083a84cb5db7a5638c5afaf4ea89dd7694
Steven Fojas <84162024+stevenfojas670@users.noreply.github.com>
2023-10-10 16:50:20 -0700

    Created as10

---
+++ as10/a10procs.asm
@@ -0,0 +1,346 @@
+; *****************************************************************
+;  Name: 
+;  NSHE_ID: 
+;  Section: 
+;  Assignment: 10
+;  Description:  
+
+; -----
+;  Function: getRadii
+;	Gets, checks, converts, and returns command line arguments.
+
+;  Function: drawSpiro()
+;	Plots spirograph formulas
+
+; ---------------------------------------------------------
+
+;	MACROS (if any) GO HERE
+
+; ---------------------------------------------------------
+
+section  .data
+
+; -----
+;  Define standard constants.
+
+TRUE		equ	1
+FALSE		equ	0
+
+SUCCESS		equ	0			; successful operation
+NOSUCCESS	equ	1
+
+STDIN		equ	0			; standard input
+STDOUT		equ	1			; standard output
+STDERR		equ	2			; standard error
+
+SYS_read	equ	0			; code for read
+SYS_write	equ	1			; code for write
+SYS_open	equ	2			; code for file open
+SYS_close	equ	3			; code for file close
+SYS_fork	equ	57			; code for fork
+SYS_exit	equ	60			; code for terminate
+SYS_creat	equ	85			; code for file open/create
+SYS_time	equ	201			; code for get time
+
+LF		equ	10
+SPACE		equ	" "
+NULL		equ	0
+ESC		equ	27
+
+; -----
+;  OpenGL constants
+
+GL_COLOR_BUFFER_BIT	equ	16384
+GL_POINTS		equ	0
+GL_POLYGON		equ	9
+GL_PROJECTION		equ	5889
+
+GLUT_RGB		equ	0
+GLUT_SINGLE		equ	0
+
+; -----
+;  Define program specific constants.
+
+R1_MIN		equ	0
+R1_MAX		equ	250			; 250(10) = 1054(6)
+
+R2_MIN		equ	1			; 1(10) = 1(13)
+R2_MAX		equ	250			; 250(10) = 1054(6)
+
+OP_MIN		equ	1			; 1(10) = 1(13)
+OP_MAX		equ	250			; 250(10) = 1054(6)
+
+SP_MIN		equ	1			; 1(10) = 1(13)
+SP_MAX		equ	100			; 100(10) = 244(6)
+
+X_OFFSET	equ	320
+Y_OFFSET	equ	240
+
+; -----
+;  Variables for getRadii procedure.
+
+errUsage	db	"Usage:  ./spiro -r1 <senary number> "
+		db	"-r2 <senary number> -op <senary number> "
+		db	"-sp <senary number> -cl <b/g/r/y/p/w>"
+		db	LF, NULL
+errBadCL	db	"Error, invalid or incomplete command line arguments."
+		db	LF, NULL
+
+errR1sp		db	"Error, radius 1 specifier incorrect."
+		db	LF, NULL
+errR1value	db	"Error, radius 1 value must be between 0 and 1054(6)."
+		db	LF, NULL
+
+errR2sp		db	"Error, radius 2 specifier incorrect."
+		db	LF, NULL
+errR2value	db	"Error, radius 2 value must be between 1 and 1054(6)."
+		db	LF, NULL
+
+errOPsp		db	"Error, offset position specifier incorrect."
+		db	LF, NULL
+errOPvalue	db	"Error, offset position value must be between 1 and 1054(6)."
+		db	LF, NULL
+
+errSPsp		db	"Error, speed specifier incorrect."
+		db	LF, NULL
+errSPvalue	db	"Error, speed value must be between 1 and 244(6)."
+		db	LF, NULL
+
+errCLsp		db	"Error, color specifier incorrect."
+		db	LF, NULL
+errCLvalue	db	"Error, color value must be b, g, r, p, or w. "
+		db	LF, NULL
+
+; -----
+;  Variables for spirograph routine.
+
+fltOne		dd	1.0
+fltZero		dd	0.0
+fltTmp1		dd	0.0
+fltTmp2		dd	0.0
+
+t		dd	0.0			; loop variable
+s		dd	1.0			; phase variable
+tStep		dd	0.005			; t step
+sStep		dd	0.0			; s step
+x		dd	0			; current x
+y		dd	0			; current y
+
+r1		dd	0.0			; radius 1 (float)
+r2		dd	0.0			; radius 2 (float)
+ofp		dd	0.0			; offset position (float)
+radii		dd	0.0			; tmp location for (radius1+radius2)
+
+scale		dd	5000.0			; speed scale
+limit		dd	360.0			; for loop limit
+iterations	dd	0			; set to 360.0/tStep
+
+red		db	0			; 0-255
+green		db	0			; 0-255
+blue		db	0			; 0-255
+
+; ------------------------------------------------------------
+
+section  .text
+
+; -----
+;  External references for openGL routines.
+
+extern	glutInit, glutInitDisplayMode, glutInitWindowSize, glutInitWindowPosition
+extern	glutCreateWindow, glutMainLoop
+extern	glutDisplayFunc, glutIdleFunc, glutReshapeFunc, glutKeyboardFunc
+extern	glutSwapBuffers, gluPerspective, glutPostRedisplay
+extern	glClearColor, glClearDepth, glDepthFunc, glEnable, glShadeModel
+extern	glClear, glLoadIdentity, glMatrixMode, glViewport
+extern	glTranslatef, glRotatef, glBegin, glEnd, glVertex3f, glColor3f
+extern	glVertex2f, glVertex2i, glColor3ub, glOrtho, glFlush, glVertex2d
+
+extern	cosf, sinf
+
+; ******************************************************************
+;  Function getRadii()
+;	Gets radius 1, radius 2, offset positionm and rottaion
+;	speedvalues and color code letter from the command line.
+
+;	Performs error checking, converts ASCII/senary string
+;	to integer.  Required ommand line format (fixed order):
+;	  "-r1 <senary numberl> -r2 <senary number> -op <senary number> 
+;			-sp <senary number> -cl <color>"
+
+; HLL
+;	stat = getRadii(argc, argv, &radius1, &radius2, &offPos,
+;						&speed, &color);
+
+; -----
+;  Arguments:
+;	- ARGC
+;	- ARGV
+;	- radius 1, double-word, address
+;	- radius 2, double-word, address
+;	- offset Position, double-word, address
+;	- speed, double-word, address
+;	- circle color, byte, address
+
+
+
+
+;	YOUR CODE GOES HERE
+
+
+
+; ******************************************************************
+;  Spirograph Plotting Function.
+
+; -----
+;  Color Code Conversion:
+;	'r' -> red=255, green=0, blue=0
+;	'g' -> red=0, green=255, blue=0
+;	'b' -> red=0, green=0, blue=255
+;	'p' -> red=255, green=0, blue=255
+;	'y' -> red=255 green=255, blue=0
+;	'w' -> red=255, green=255, blue=255
+;  Note, set color before plot loop.
+
+; -----
+;  The loop is from 0.0 to 360.0 by tStep, can calculate
+;  the number if iterations via:  iterations = 360.0 / tStep
+;  This eliminates needing a float compare (a hassle).
+
+; -----
+;  Basic flow:
+;	Set openGL drawing initializations
+;	Loop initializations
+;		Set draw color (i.e., glColor3ub)
+;		Convert integer values to float for calculations
+;		set 'sStep' variable
+;		set 'iterations' variable
+;	Plot the following spirograph equations:
+;	     for (t=0.0; t<360.0; t+=step) {
+;	         radii = (r1+r2)
+;	         x = (radii * cos(t)) + (offPos * cos(radii * ((t+s)/r2)))
+;	         y = (radii * sin(t)) + (offPos * sin(radii * ((t+s)/r2)))
+;	         t += tStep
+;	         plot point (x, y)
+;	     }
+;	Close openGL plotting (i.e., glEnd and glFlush)
+;	Update s for next call (s += sStep)
+;	Ensure openGL knows to call again (i.e., glutPostRedisplay)
+
+; -----
+;  The animation is accomplished by plotting a static
+;	image, exiting the routine, and replotting a new
+;	slightly different image.  The 's' variable controls
+;	the phase or animation.
+
+; -----
+;  Global variables accessed
+;	There are defined and set in the main, accessed herein by
+;	name as per the below declarations.
+
+common	radius1		1:4		; radius 1, dword, integer value
+common	radius2		1:4		; radius 2, dword, integer value
+common	offPos		1:4		; offset position, dword, integer value
+common	speed		1:4		; rortation speed, dword, integer value
+common	color		1:1		; color code letter, byte, ASCII value
+
+global drawSpiro
+drawSpiro:
+	push	r12
+
+; -----
+;  Prepare for drawing
+	; glClear(GL_COLOR_BUFFER_BIT);
+	mov	rdi, GL_COLOR_BUFFER_BIT
+	call	glClear
+
+	; glBegin();
+	mov	rdi, GL_POINTS
+	call	glBegin
+
+; -----
+;  Set draw color(r,g,b)
+;	Convert color letter to color values
+;	Note, only legal color letters should be
+;		passed to this procedure
+;	Note, color values should be store in local
+;		variables red, green, and blue
+
+;	YOUR CODE GOES HERE
+
+
+
+; -----
+;  Loop initializations and main plotting loop
+
+;	YOUR CODE GOES HERE
+
+
+; -----
+;  Plotting done.
+
+	call	glEnd
+	call	glFlush
+
+; -----
+;  Update s for next call.
+
+;	YOUR CODE GOES HERE
+
+
+; -----
+;  Ensure openGL knows to call again
+
+	call	glutPostRedisplay
+
+	pop	r12
+	ret
+
+; ******************************************************************
+;  Generic function to display a string to the screen.
+;  String must be NULL terminated.
+
+;  Algorithm:
+;	Count characters in string (excluding NULL)
+;	Use syscall to output characters
+
+;  Arguments:
+;	- address, string
+;  Returns:
+;	nothing
+
+global	printString
+printString:
+
+; -----
+;  Count characters in string.
+
+	mov	rbx, rdi			; str addr
+	mov	rdx, 0
+strCountLoop:
+	cmp	byte [rbx], NULL
+	je	strCountDone
+	inc	rbx
+	inc	rdx
+	jmp	strCountLoop
+strCountDone:
+
+	cmp	rdx, 0
+	je	prtDone
+
+; -----
+;  Call OS to output string.
+
+	mov	rax, SYS_write			; system code for write()
+	mov	rsi, rdi			; address of characters to write
+	mov	rdi, STDOUT			; file descriptor for standard in
+						; EDX=count to write, set above
+	syscall					; system call
+
+; -----
+;  String printed, return to calling routine.
+
+prtDone:
+	ret
+
+; ******************************************************************
+
